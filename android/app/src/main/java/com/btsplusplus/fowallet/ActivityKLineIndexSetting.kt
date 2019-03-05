9f684e76340a366f1ebf7281f3bc16bd6cbb1355
syalon <hanomirin@foxmail.com>
2019-03-05 16:57:56 +0800

    android: Improve K-line index setting

--- android/app/src/main/java/com/btsplusplus/fowallet/ActivityKLineIndexSetting.kt
+++ android/app/src/main/java/com/btsplusplus/fowallet/ActivityKLineIndexSetting.kt
@@ -1,30 +1,26 @@
 package com.btsplusplus.fowallet
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.app.AlertDialog
-import android.widget.LinearLayout
+import android.view.View
 import android.widget.TextView
-import bitshares.Promise
-import bitshares.SettingManager
-import bitshares.TempManager
-import bitshares.forEach
+import bitshares.*
 import kotlinx.android.synthetic.main.activity_kline_quota_setting.*
 import org.json.JSONArray
 import org.json.JSONObject
 
 class ActivityKLineIndexSetting : BtsppActivity() {
 
-    private lateinit var m_layout_ma: LinearLayout
-    private lateinit var m_layout_boll: LinearLayout
-
-    private lateinit var m_dialog_main: AlertDialog
-    private lateinit var m_dialog_select: AlertDialog
-
     private lateinit var _result_promise: Promise
-    // private var _bResultCannelled: Boolean = false
     private lateinit var _picker_data_array: MutableList<Int>
     private lateinit var _configValueHash: JSONObject
+
+    private lateinit var _main_index_type_array: JSONArray
+    private lateinit var _sub_index_type_array: JSONArray
+
+    override fun onBackClicked(result: Any?) {
+        _result_promise.resolve(false)
+        super.onBackClicked(result)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,184 +29,272 @@ class ActivityKLineIndexSetting : BtsppActivity() {
         // 设置全屏(隐藏状态栏和虚拟导航栏)
         setFullScreen()
 
-        //_bResultCannelled = true
+        //  get params
         _picker_data_array = mutableListOf()
         val args = TempManager.sharedTempManager().get_args_as_JSONObject()
         _result_promise = args.get("result_promise") as Promise
 
-        //val j = SettingManager.sharedSettingManager().getKLineIndexInfos().toString()
         _configValueHash = JSONObject(SettingManager.sharedSettingManager().getKLineIndexInfos().toString())
 
-        m_layout_ma = layout_ma_setting_of_kline_quota_setting
-        m_layout_boll = layout_boll_setting_of_kline_quota_setting
+        _main_index_type_array = jsonArrayfrom(jsonObjectfromKVS("name", resources.getString(R.string.kKlineIndexCellHide), "value", ""),
+                jsonObjectfromKVS("name", "MA", "value", "ma"),
+                jsonObjectfromKVS("name", "EMA", "value", "ema"),
+                jsonObjectfromKVS("name", "BOLL", "value", "boll"))
 
-        loadSettingAndRefreshUI()
+        _sub_index_type_array = jsonArrayfrom(jsonObjectfromKVS("name", resources.getString(R.string.kKlineIndexCellHide), "value", ""),
+                jsonObjectfromKVS("name", "MACD", "value", "macd"))
 
-        layout_back_from_kline_quota_setting.setOnClickListener{
-            finish()
-        }
-        layout_main_view_of_kline_quota_setting.setOnClickListener{
-            onMainViewClick()
-        }
-        layout_ma1_of_kline_quota_setting.setOnClickListener{
-            onMaViewClick(0)
-        }
-        layout_ma2_of_kline_quota_setting.setOnClickListener{
-            onMaViewClick(1)
-        }
-        layout_ma3_of_kline_quota_setting.setOnClickListener{
-            onMaViewClick(2)
-        }
-        layout_bolln_of_kline_quota_setting.setOnClickListener{
-            onBollViewClick(0)
-        }
-        layout_bollp_of_kline_quota_setting.setOnClickListener{
-            onBollViewClick(1)
+        //  refresh ui
+        refreshIndexAll()
+
+        layout_back_from_kline_quota_setting.setOnClickListener{ onBackClicked(false) }
+        layout_main.setOnClickListener{ onMainViewClick() }
+        layout_sub.setOnClickListener { onSubViewClick() }
+        button_commit.setOnClickListener { onCommitClicked() }
+
+        //  index binding events
+        layout_main_index01.setOnClickListener { onMainIndexClicked(0) }
+        layout_main_index02.setOnClickListener { onMainIndexClicked(1) }
+        layout_main_index03.setOnClickListener { onMainIndexClicked(2) }
+        layout_sub_index01.setOnClickListener { onSubIndexClicked(0) }
+        layout_sub_index02.setOnClickListener { onSubIndexClicked(1) }
+        layout_sub_index03.setOnClickListener { onSubIndexClicked(2) }
+    }
+
+    private fun onCommitClicked(){
+        SettingManager.sharedSettingManager().setUseConfig(kSettingKey_KLineIndexInfo, _configValueHash)
+        _result_promise.resolve(true)
+        finish()
+    }
+
+    private fun onMainIndexClicked(row: Int){
+        val value_type = _configValueHash.optString("kMain")
+        if (value_type == "ma"){
+            onSelectIndexMA(row)
+        }else if (value_type == "ema"){
+            onSelectIndexEMA(row)
+        }else if (value_type == "boll"){
+            onSelectIndexBOLL(row)
         }
     }
 
-    private fun loadSettingAndRefreshUI(){
-        hideMaAndBollLayout()
-        val main_value = _configValueHash.optString("kMain")
-        if ( main_value == null || main_value.equals("") ){
-            tv_main_view_of_kline_quota_setting.text = resources.getString(R.string.klineQuotaSettingPageTextNotDisplay)
-        } else {
-            val value_values = _configValueHash.getJSONArray("${main_value}_value")
+    private fun onSubIndexClicked(row: Int){
+        val value_type = _configValueHash.optString("kSub")
+        if (value_type == "macd"){
+            onSelectIndexMACD(row)
+        }
+    }
 
-            tv_main_view_of_kline_quota_setting.text = main_value.toUpperCase()
-            if ( main_value.equals("ma") ) {
-                m_layout_ma.visibility = LinearLayout.VISIBLE
+    private fun onSelectNumberFromRange(bgn: Int, end: Int): Promise{
+        val p = Promise()
 
-                var _index = 0
-                value_values.forEach<Int> {
-
-                    when (_index) {
-                        0 -> {
-                            tv_ma1_value_of_kline_quota_setting.text = _index.toString()
-                        }
-                        1 -> {
-                            tv_ma2_value_of_kline_quota_setting.text = _index.toString()
-                        }
-                        2 -> {
-                            tv_ma3_value_of_kline_quota_setting.text = _index.toString()
-                        }
-                    }
-                    _index++
-                }
+        val nameList = JSONArray()
+        val valueList = JSONArray()
+        for (i in bgn..end) {
+            if (i == 0){
+                nameList.put(resources.getString(R.string.kKlineIndexCellHide))
+            }else{
+                nameList.put(i.toString())
             }
-            if ( main_value.equals("boll") ) {
-                m_layout_boll.visibility = LinearLayout.VISIBLE
+            valueList.put(i)
+        }
+        ViewSelector.show(this,"", nameList.toList<String>().toTypedArray()){ _index: Int, _: String ->
+            p.resolve(valueList.getInt(_index))
+        }
 
-                var _index = 0
-                value_values.forEach<Int> {
+        return p
+    }
 
-                    when (_index) {
-                        0 -> {
-                            tv_bolln_value_of_kline_quota_setting.text = _index.toString()
-                        }
-                        1 -> {
-                            tv_bollp_value_of_kline_quota_setting.text = _index.toString()
-                        }
-                    }
-                    _index++
-                }
+    private fun onSelectIndexMA(row: Int){
+        onSelectNumberFromRange(0, 90).then {
+            val value = it as Int
+            _configValueHash.getJSONArray("ma_value").put(row, value)
+            when (row) {
+                0 -> refreshIndexValueLabel(main_index01_value, value)
+                1 -> refreshIndexValueLabel(main_index02_value, value)
+                2 -> refreshIndexValueLabel(main_index03_value, value)
             }
+            return@then null
         }
     }
 
-    /**
-     *  (private) 核心 确认交易，发送。
-     */
-    private fun onCommitCore(){
-        val sConfigValueHash = _configValueHash.toString()
-        SettingManager.sharedSettingManager().setUseConfig("kSettingKey_KLineIndexInfo",sConfigValueHash)
-        // _bResultCannelled = false
+    private fun onSelectIndexEMA(row: Int){
+        onSelectNumberFromRange(0, 90).then {
+            val value = it as Int
+            _configValueHash.getJSONArray("ema_value").put(row, value)
+            when (row) {
+                0 -> refreshIndexValueLabel(main_index01_value, value)
+                1 -> refreshIndexValueLabel(main_index02_value, value)
+                2 -> refreshIndexValueLabel(main_index03_value, value)
+            }
+            return@then null
+        }
     }
 
-    private fun buildPickData(is_ma_picker: Boolean) : Array<String> {
-        val ma_value_min = 2
-        val ma_value_max = 91
-        var _pick_index = 0
-        val data_arr : Array<String>
-
-        if ( is_ma_picker ) {
-            data_arr = Array(90, {""})
-            data_arr[0] = "0"  //  0 means 'hide'
-            _pick_index = 1
-        } else {
-            data_arr = Array(89, {""})
+    private fun onSelectIndexBOLL(row: Int){
+        val key: String
+        val bgn: Int
+        val end: Int
+        if (row == 0){
+            key = "n"
+            bgn = 1
+            end = 90
+        }else{
+            key = "p"
+            bgn = 1
+            end = 9
         }
-        for ( ma_value in ma_value_min until ma_value_max ) {
-            data_arr[_pick_index] = ma_value.toString()
-            _pick_index++
+        onSelectNumberFromRange(bgn, end).then {
+            val value = it as Int
+            _configValueHash.getJSONObject("boll_value").put(key, value)
+            when (row) {
+                0 -> main_index01_value.text = value.toString()
+                1 -> main_index02_value.text = value.toString()
+            }
+            return@then null
         }
-        return data_arr
     }
 
-
-    private fun onMaViewClick(select_index: Int){
-
-        m_dialog_select = ViewSelector.show(this,"", buildPickData(true)){ _index: Int, result: String ->
-
-            when (select_index) {
+    private fun onSelectIndexMACD(row: Int){
+        onSelectNumberFromRange(2, 90).then {
+            val value = it as Int
+            when (row) {
                 0 -> {
-                    tv_ma1_value_of_kline_quota_setting.text = result
+                    _configValueHash.getJSONObject("macd_value").put("s", value)
+                    sub_index01_value.text = value.toString()
                 }
                 1 -> {
-                    tv_ma2_value_of_kline_quota_setting.text = result
+                    _configValueHash.getJSONObject("macd_value").put("l", value)
+                    sub_index02_value.text = value.toString()
                 }
                 2 -> {
-                    tv_ma3_value_of_kline_quota_setting.text = result
+                    _configValueHash.getJSONObject("macd_value").put("m", value)
+                    sub_index03_value.text = value.toString()
                 }
             }
-            _configValueHash.getJSONArray("ma_value").put(select_index,result.toInt())
-
-            m_dialog_select.dismiss()
+            return@then null
         }
     }
 
-    private fun onBollViewClick(select_index: Int){
+    private fun refreshIndexAll(){
+        refreshMainIndexAll()
+        refreshSubIndexAll()
+    }
 
-        m_dialog_select = ViewSelector.show(this,"", buildPickData(false)){ _index: Int, result: String ->
-
-            when (select_index) {
-                0 -> {
-                    _configValueHash.getJSONObject("boll_value").put("n", result.toInt())
-                    tv_bolln_value_of_kline_quota_setting.text = result
-                }
-                1 -> {
-                    _configValueHash.getJSONObject("boll_value").put("p", result.toInt())
-                    tv_bollp_value_of_kline_quota_setting.text = result
-                }
-            }
-            m_dialog_select.dismiss()
+    private fun refreshIndexValueLabel(label: TextView, value: Int){
+        if (value > 0){
+            label.text = value.toString()
+        }else{
+            label.text = resources.getString(R.string.kKlineIndexCellHide)
         }
     }
 
+    private fun refreshMainIndexAll(){
+        layout_main_index01.visibility = View.GONE
+        layout_main_index02.visibility = View.GONE
+        layout_main_index03.visibility = View.GONE
+
+        val value_type = _configValueHash.optString("kMain")
+        if (value_type == ""){
+            layout_main_index_all.visibility = View.GONE
+            layout_main_value.text = resources.getString(R.string.kKlineIndexCellHide)
+        }else{
+            layout_main_index_all.visibility = View.VISIBLE
+            layout_main_value.text = value_type.toUpperCase()
+            if (value_type == "ma"){
+                val value_values = _configValueHash.getJSONArray("${value_type}_value")
+                assert(value_values.length() == 3)
+                layout_main_index01.visibility = View.VISIBLE
+                layout_main_index02.visibility = View.VISIBLE
+                layout_main_index03.visibility = View.VISIBLE
+                main_index01_name.text = "MA1"
+                main_index02_name.text = "MA2"
+                main_index03_name.text = "MA3"
+                main_index01_name.setTextColor(resources.getColor(R.color.theme01_ma5Color))
+                main_index02_name.setTextColor(resources.getColor(R.color.theme01_ma10Color))
+                main_index03_name.setTextColor(resources.getColor(R.color.theme01_ma30Color))
+                refreshIndexValueLabel(main_index01_value, value_values.getInt(0))
+                refreshIndexValueLabel(main_index02_value, value_values.getInt(1))
+                refreshIndexValueLabel(main_index03_value, value_values.getInt(2))
+            }else if (value_type == "ema"){
+                val value_values = _configValueHash.getJSONArray("${value_type}_value")
+                assert(value_values.length() == 3)
+                layout_main_index01.visibility = View.VISIBLE
+                layout_main_index02.visibility = View.VISIBLE
+                layout_main_index03.visibility = View.VISIBLE
+                main_index01_name.text = "EMA1"
+                main_index02_name.text = "EMA2"
+                main_index03_name.text = "EMA3"
+                main_index01_name.setTextColor(resources.getColor(R.color.theme01_ma5Color))
+                main_index02_name.setTextColor(resources.getColor(R.color.theme01_ma10Color))
+                main_index03_name.setTextColor(resources.getColor(R.color.theme01_ma30Color))
+                refreshIndexValueLabel(main_index01_value, value_values.getInt(0))
+                refreshIndexValueLabel(main_index02_value, value_values.getInt(1))
+                refreshIndexValueLabel(main_index03_value, value_values.getInt(2))
+            }else if (value_type == "boll"){
+                val value_values = _configValueHash.getJSONObject("${value_type}_value")
+                assert(value_values.length() == 2)
+                layout_main_index01.visibility = View.VISIBLE
+                layout_main_index02.visibility = View.VISIBLE
+                main_index01_name.text = resources.getString(R.string.kKlineIndexCellBollN)
+                main_index02_name.text = resources.getString(R.string.kKlineIndexCellBollP)
+                main_index01_name.setTextColor(resources.getColor(R.color.theme01_textColorMain))
+                main_index02_name.setTextColor(resources.getColor(R.color.theme01_textColorMain))
+                main_index01_value.text = value_values.getInt("n").toString()
+                main_index02_value.text = value_values.getInt("p").toString()
+            }else{
+                assert(false)
+            }
+        }
+    }
+
+    private fun refreshSubIndexAll(){
+        layout_sub_index01.visibility = View.GONE
+        layout_sub_index02.visibility = View.GONE
+        layout_sub_index03.visibility = View.GONE
+
+        val value_type = _configValueHash.optString("kSub")
+        if (value_type == ""){
+            layout_sub_index_all.visibility = View.GONE
+            layout_sub_value.text = resources.getString(R.string.kKlineIndexCellHide)
+        }else{
+            layout_sub_index_all.visibility = View.VISIBLE
+            layout_sub_value.text = value_type.toUpperCase()
+            if (value_type == "macd"){
+                val value_values = _configValueHash.getJSONObject("${value_type}_value")
+                assert(value_values.length() == 3)
+                layout_sub_index01.visibility = View.VISIBLE
+                layout_sub_index02.visibility = View.VISIBLE
+                layout_sub_index03.visibility = View.VISIBLE
+                sub_index01_name.text = resources.getString(R.string.kKlineIndexCellMacdS)
+                sub_index02_name.text = resources.getString(R.string.kKlineIndexCellMacdL)
+                sub_index03_name.text = resources.getString(R.string.kKlineIndexCellMacdM)
+                sub_index01_value.text = value_values.getInt("s").toString()
+                sub_index02_value.text = value_values.getInt("l").toString()
+                sub_index03_value.text = value_values.getInt("m").toString()
+            }else{
+                assert(false)
+            }
+        }
+    }
 
     private fun onMainViewClick(){
-        m_dialog_main = ViewSelector.show(this,"", arrayOf(resources.getString(R.string.klineQuotaSettingPageTextNotDisplay),"MA","BOLL")){ index: Int, result: String ->
-            hideMaAndBollLayout()
-            tv_main_view_of_kline_quota_setting.text = result
-            when (index) {
-                0 -> {
-                    _configValueHash.put("kMain","")
-                }
-                1 -> {
-                    _configValueHash.put("kMain","ma")
-                    m_layout_ma.visibility = LinearLayout.VISIBLE
-                }
-                2 -> {
-                    _configValueHash.put("kMain","boll")
-                    m_layout_boll.visibility = LinearLayout.VISIBLE
-                }
-            }
-            m_dialog_main.dismiss()
+        val list = JSONArray()
+        _main_index_type_array.forEach<JSONObject> { list.put(it!!.getString("name")) }
+        ViewSelector.show(this, resources.getString(R.string.kKlineIndexSelectMainIndex), list.toList<String>().toTypedArray()){ index: Int, result: String ->
+            layout_main_value.text = result
+            _configValueHash.put("kMain", _main_index_type_array.getJSONObject(index).getString("value"))
+            refreshMainIndexAll()
         }
     }
 
-    private fun hideMaAndBollLayout() {
-        m_layout_ma.visibility = LinearLayout.GONE
-        m_layout_boll.visibility = LinearLayout.GONE
+    private fun onSubViewClick(){
+        val list = JSONArray()
+        _sub_index_type_array.forEach<JSONObject> { list.put(it!!.getString("name")) }
+        ViewSelector.show(this, resources.getString(R.string.kKlineIndexSelectSubIndex), list.toList<String>().toTypedArray()){ index: Int, result: String ->
+            layout_sub_value.text = result
+            _configValueHash.put("kSub", _sub_index_type_array.getJSONObject(index).getString("value"))
+            refreshSubIndexAll()
+        }
     }
 }
