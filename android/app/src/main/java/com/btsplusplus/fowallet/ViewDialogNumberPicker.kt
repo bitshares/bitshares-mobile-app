f20a11ca37696dd9feb179295ebb2f94e7ad3a8b
syalon <hanomirin@foxmail.com>
2019-03-06 12:51:02 +0800

    android: Improved ViewDialogNumberPicker controls

--- android/app/src/main/java/com/btsplusplus/fowallet/ViewDialogNumberPicker.kt
+++ android/app/src/main/java/com/btsplusplus/fowallet/ViewDialogNumberPicker.kt
@@ -2,17 +2,19 @@ package com.btsplusplus.fowallet
 
 import android.app.Dialog
 import android.content.Context
+import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.util.AttributeSet
+import android.util.TypedValue
 import android.view.*
 import android.widget.*
 import bitshares.LLAYOUT_MATCH
 import bitshares.LLAYOUT_WARP
 import bitshares.dp
+import kotlin.math.max
 
-
-class NumberPickerBtspp : NumberPicker {
+class BtsppNumberPicker : NumberPicker {
 
     constructor(ctx: Context) : super(ctx)
     constructor(ctx: Context, attrs: AttributeSet) : super(ctx, attrs)
@@ -44,8 +46,8 @@ class NumberPickerBtspp : NumberPicker {
     }
 
     private fun updateView(view: View?) {
-        if ( view is EditText ) {
-            ( view as EditText ).apply {
+        if (view is EditText) {
+            view.apply {
                 setTextColor(resources.getColor(R.color.theme01_textColorMain))
                 setFocusable(false)
             }
@@ -55,61 +57,82 @@ class NumberPickerBtspp : NumberPicker {
 
 class ViewDialogNumberPicker : Dialog {
 
-    constructor(ctx: Context, array_data: Array<String>, callback: (index: Int, result: String) -> Unit) : super(ctx) {
-        var _ctx = ctx
-        var _selected_value = 0
-
-        // 外层 Layout
+    constructor(ctx: Context, title:String?, array_data: Array<String>, default_selected:Int, callback: (index: Int, result: String) -> Unit) : super(ctx) {
+        //  外层 Layout
         val layout = LinearLayout(context)
         val layout_params = LinearLayout.LayoutParams(100.dp, 100.dp)
+        layout_params.leftMargin = 0
+        layout_params.rightMargin = 0
         layout.orientation = LinearLayout.VERTICAL
         layout.layoutParams = layout_params
-        layout.setBackgroundColor(context!!.resources.getColor(R.color.theme01_appBackColor))
-        layout.setPadding(20, 20, 20, 20)
+        layout.setBackgroundColor(context.resources.getColor(R.color.theme01_appBackColor))
+        layout.setPadding(0, 0, 0, 0)
 
-        // 顶部按钮(取消 和 确认)
+        //  顶部按钮(取消 和 确认)
         val layout_buttons = LinearLayout(context).apply {
-            layoutParams = LinearLayout.LayoutParams(LLAYOUT_MATCH, LLAYOUT_WARP)
+            layoutParams = LinearLayout.LayoutParams(LLAYOUT_MATCH, 44.dp)
             orientation = LinearLayout.HORIZONTAL
+            setPadding(0, 0, 0, 0)
+            setBackgroundColor(context.resources.getColor(R.color.theme01_tabBarColor))
         }
 
-        val tv_cancel = TextView(_ctx).apply {
-            layoutParams = LinearLayout.LayoutParams(LLAYOUT_MATCH, LLAYOUT_WARP,1f)
+        val tv_cancel = TextView(ctx).apply {
+            layoutParams = LinearLayout.LayoutParams(0, LLAYOUT_WARP,1f).apply {
+                gravity = Gravity.CENTER_VERTICAL
+            }
             gravity = Gravity.LEFT or Gravity.CENTER_VERTICAL
-            textSize = 7.5f.dp
-            text = _ctx.resources.getString(R.string.nameCancel)
-            setTextColor(_ctx.resources.getColor(R.color.theme01_textColorMain))
+            this.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 13.0f)
+            text = ctx.resources.getString(R.string.nameCancel)
+            setTextColor(ctx.resources.getColor(R.color.theme01_textColorMain))
+            setPadding(10.dp, 0, 0, 0)
         }
 
-        val tv_ok = TextView(_ctx).apply {
-            layoutParams = LinearLayout.LayoutParams(LLAYOUT_MATCH, LLAYOUT_WARP, 1f)
+        var tv_title:TextView? = null
+        if (title != null && title.isNotEmpty()){
+            tv_title = TextView(ctx).apply {
+                layoutParams = LinearLayout.LayoutParams(0, LLAYOUT_WARP, 2f).apply {
+                    gravity = Gravity.CENTER_VERTICAL
+                }
+                gravity = Gravity.CENTER or Gravity.CENTER_VERTICAL
+                this.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 13.0f)
+                text = title
+                setTextColor(ctx.resources.getColor(R.color.theme01_textColorMain))
+                setPadding(0, 0, 10.dp, 0)
+            }
+        }
+
+        val tv_ok = TextView(ctx).apply {
+            layoutParams = LinearLayout.LayoutParams(0, LLAYOUT_WARP, 1f).apply {
+                gravity = Gravity.CENTER_VERTICAL
+            }
             gravity = Gravity.RIGHT or Gravity.CENTER_VERTICAL
-            textSize = 7.5f.dp
-            text = _ctx.resources.getString(R.string.nameOk)
-            setTextColor(_ctx.resources.getColor(R.color.theme01_textColorMain))
+            this.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 13.0f)
+            text = ctx.resources.getString(R.string.nameOk)
+            setTextColor(ctx.resources.getColor(R.color.theme01_textColorMain))
+            setPadding(0, 0, 10.dp, 0)
         }
 
-        val picker = NumberPickerBtspp(context)
+        val picker = BtsppNumberPicker(context)
         picker.displayedValues = array_data
         picker.minValue = 0
         picker.maxValue = array_data.size - 1
-        picker.value = 0
-        picker.layoutParams = LinearLayout.LayoutParams(LLAYOUT_MATCH, LLAYOUT_WARP )
+        picker.value = max(default_selected, 0)
+        picker.layoutParams = LinearLayout.LayoutParams(LLAYOUT_MATCH, LLAYOUT_WARP)
 
-        // 更改分割线颜色和高度（利用反射修改属性）
+        //  更改分割线颜色和高度（利用反射修改属性）
         picker.javaClass.superclass.declaredFields.forEach {
-            if ( it.name.equals("mSelectionDivider") ) {
+            if (it.name == "mSelectionDivider") {
                 it.isAccessible = true
                 try {
-                    it.set(picker, ColorDrawable(_ctx.resources.getColor(R.color.theme01_textColorGray)))
+                    it.set(picker, ColorDrawable(ctx.resources.getColor(R.color.theme01_bottomLineColor)))
                 } catch ( e: Exception ) {
                     e.printStackTrace()
                 }
             }
-            if ( it.name.equals("mSelectionDividerHeight" )){
+            if (it.name == "mSelectionDividerHeight"){
                 it.isAccessible = true
                 try {
-                    it.set(picker, 1)
+                    it.set(picker, 1.dp)
                 } catch ( e: Exception ) {
                     e.printStackTrace()
                 }
@@ -117,51 +140,41 @@ class ViewDialogNumberPicker : Dialog {
         }
 
         layout_buttons.addView(tv_cancel)
+        tv_title?.let { layout_buttons.addView(it) }
         layout_buttons.addView(tv_ok)
         layout.addView(layout_buttons)
         layout.addView(picker)
 
         setContentView(layout)
-        setCanceledOnTouchOutside(false)
-        setCancelable(false)
 
-        // REMARK 部分机型去除标题栏
+        //  REMARK 部分机型去除标题栏
         val v = this.findViewById<View>(android.R.id.title)
         v?.visibility = View.GONE
 
-        // 取消点击
+        //  取消点击
         tv_cancel.setOnClickListener{
             dismiss()
         }
 
-        // 确认点击
+        //  确认点击
         tv_ok.setOnClickListener{
-            callback.invoke(_selected_value,array_data[_selected_value])
+            val selected = picker.value
+            callback.invoke(selected, array_data[selected])
             dismiss()
-        }
-
-        // number picker 选择
-        picker.setOnValueChangedListener{ number_picker: NumberPicker, old_val: Int, new_val: Int ->
-            _selected_value = new_val
-        }
-
-        //  禁止系统返回键
-        this.setOnKeyListener { dialogInterface, keyCode, keyEvent ->
-            if (keyCode == KeyEvent.KEYCODE_BACK) {
-                return@setOnKeyListener false
-            }
-            return@setOnKeyListener true
         }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        val window = this.window
-        window.setGravity(Gravity.CENTER_VERTICAL)
+        val window = this.window!!
+        //  bottom
+        window.setGravity(Gravity.BOTTOM)
+        //  full screen
+        window.decorView.setPadding(0, 0, 0, 0)
         val params: WindowManager.LayoutParams  = window.attributes
         params.width = WindowManager.LayoutParams.MATCH_PARENT
         params.height = WindowManager.LayoutParams.WRAP_CONTENT
         window.attributes = params
+        window.decorView.setBackgroundColor(Color.GREEN)
     }
 }
