ece4c4d77712a7eae6d5a27894d14cbc5818a2e2
syalon <hanomirin@foxmail.com>
2019-03-08 15:39:41 +0800

    android：Fixed issue #67 @fabric

--- android/app/src/main/java/com/btsplusplus/fowallet/UtilsCurveSlider.kt
+++ android/app/src/main/java/com/btsplusplus/fowallet/UtilsCurveSlider.kt
@@ -58,10 +58,12 @@ class UtilsCurveSlider {
 
         // REMARK 调整 条和块的颜色
         val color = (_slider.context as AppCompatActivity).resources.getColor(R.color.theme01_textColorMain)
-        val layerDrawable = _slider.getProgressDrawable() as LayerDrawable
-        val dra = layerDrawable.getDrawable(2)
-        dra.setColorFilter(color, PorterDuff.Mode.SRC)
-        _slider.getThumb().setColorFilter(color, PorterDuff.Mode.SRC_ATOP)
+        val layerDrawable = _slider.progressDrawable as? LayerDrawable
+        if (layerDrawable != null){
+            val dra = layerDrawable.getDrawable(2)
+            dra.setColorFilter(color, PorterDuff.Mode.SRC)
+        }
+        _slider.thumb.setColorFilter(color, PorterDuff.Mode.SRC_ATOP)
         _slider.invalidate()
 
         _slider.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
