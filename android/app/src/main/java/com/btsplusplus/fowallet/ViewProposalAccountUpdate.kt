3fd1104bb991a72cb0758335b78051f4dd460fff
qingfengzh <yoshikizh@126.com>
2019-05-10 20:37:47 +0800

    android: Improve the proposal update account interface display details.

---
+++ android/app/src/main/java/com/btsplusplus/fowallet/ViewProposalAccountUpdate.kt
@@ -0,0 +1,128 @@
+package com.btsplusplus.fowallet
+
+import android.content.Context
+import android.util.TypedValue
+import android.view.Gravity
+import android.widget.LinearLayout
+import android.widget.TextView
+import bitshares.LLAYOUT_MATCH
+import bitshares.LLAYOUT_WARP
+import bitshares.dp
+import bitshares.xmlstring
+import com.fowallet.walletcore.bts.ChainObjectManager
+import org.json.JSONObject
+
+class ViewProposalAccountUpdate : LinearLayout {
+
+    var _ctx: Context
+    var _item: JSONObject
+    var _useBuyColorForTitle: Boolean
+
+    val content_fontsize = 11.0f
+
+    constructor(ctx: Context, item: JSONObject, useBuyColorForTitle: Boolean) : super(ctx) {
+        _ctx = ctx
+        _item = item
+        _useBuyColorForTitle = useBuyColorForTitle
+        val layout_params = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT)
+        layout_params.topMargin = 10
+        this.layoutParams = layout_params
+        this.orientation = LinearLayout.VERTICAL
+
+        createUI()
+    }
+
+    fun createUI(): LinearLayout {
+
+        val opdata = _item.getJSONObject("opdata")
+        val opaccount = ChainObjectManager.sharedChainObjectManager().getChainObjectByID(opdata.getString("account"))
+
+        val new_owner = opdata.optJSONObject("owner")
+        val new_active = opdata.optJSONObject("active")
+        val new_options =  opdata.optJSONObject("new_options")
+
+        val uidata = _item.getJSONObject("uidata")
+
+        // 第一行 提案名称 【危险操作】
+        val layout_wrap = LinearLayout(_ctx)
+        layout_wrap.layoutParams = LinearLayout.LayoutParams(LLAYOUT_MATCH, LLAYOUT_WARP)
+        layout_wrap.orientation = LinearLayout.HORIZONTAL
+        layout_wrap.gravity = Gravity.CENTER_VERTICAL
+        layout_wrap.apply {
+            val tv1 = TextView(_ctx).apply {
+                layoutParams = LinearLayout.LayoutParams(LLAYOUT_WARP, LLAYOUT_WARP)
+                gravity = Gravity.LEFT or Gravity.CENTER_VERTICAL
+                if (_useBuyColorForTitle){
+                    setTextColor(resources.getColor(R.color.theme01_buyColor))
+                } else {
+                    val _color = uidata.getInt("color")
+                    setTextColor(resources.getColor(_color))
+                }
+                setTextSize(TypedValue.COMPLEX_UNIT_DIP, content_fontsize)
+                text = uidata.getString("name")
+            }
+            addView(tv1)
+
+            if (new_owner != null || new_active != null){
+                val tv2 = TextView(_ctx).apply {
+                    layoutParams = LinearLayout.LayoutParams(LLAYOUT_WARP, LLAYOUT_WARP).apply {
+                        setMargins(5.dp,0.dp,0.dp,0.dp)
+                        setPadding(5.dp,0,5.dp,0)
+                        gravity = Gravity.LEFT or Gravity.CENTER_VERTICAL
+                    }
+                    gravity = Gravity.LEFT or Gravity.CENTER_VERTICAL
+                    setTextColor(resources.getColor(R.color.theme01_textColorMain))
+                    setBackgroundColor(resources.getColor(R.color.theme01_sellColor))
+                    setTextSize(TypedValue.COMPLEX_UNIT_DIP, 10.0f)
+                    text = R.string.kOpDetailFlagDangerous.xmlstring(_ctx)
+                }
+                addView(tv2)
+            }
+        }
+        addView(layout_wrap)
+
+        //  第二行 特殊View or 没有任何更新。
+        //  1、所有者权限
+        var _viewPermissionOwner: LinearLayout? = null
+        var _viewPermissionActive: LinearLayout? = null
+        var _viewPermissionMemoKey: LinearLayout? = null
+        var _viewVotingInfos: LinearLayout? = null
+        if (new_owner != null){
+            _viewPermissionOwner = ViewProposalAccountUpdatePermissionOwner(_ctx).initWithPermission(opaccount.getJSONObject("owner"), new_owner, R.string.kOpDetailPermissionOwner.xmlstring(_ctx))
+            addView(_viewPermissionOwner)
+        }
+
+        //  2、资金权限
+        if (new_active != null){
+            _viewPermissionActive = ViewProposalAccountUpdatePermissionOwner(_ctx).initWithPermission(opaccount.getJSONObject("active"), new_active, R.string.kOpDetailPermissionActive.xmlstring(_ctx))
+            addView(_viewPermissionActive)
+        }
+
+        if (new_options != null){
+            val old_options = opaccount.getJSONObject("options")
+            
+            //  3、备注权限
+            val old_memo_key = old_options.getString("memo_key")
+            val new_memo_key = new_options.getString("memo_key")
+            if (!old_memo_key.equals(new_memo_key)){
+                _viewPermissionMemoKey = ViewProposalAccountUpdateMemoKey(_ctx).initWithOldMemo(old_memo_key, new_memo_key, R.string.kOpDetailPermissionMemo.xmlstring(_ctx))
+                addView(_viewPermissionMemoKey)
+            }
+
+            //  4、投票信息（包括代理）
+            _viewVotingInfos = ViewProposalAccountUpdateVoting(_ctx).initWithOptions(old_options,new_options)
+            if (_viewVotingInfos != null){
+                addView(_viewVotingInfos)
+            }
+        }
+
+        if (_viewPermissionOwner != null || _viewPermissionActive != null || _viewPermissionMemoKey != null || _viewVotingInfos != null){
+            // REMARK ios 有初始化需要隐藏 ，安卓没初始化
+        }else{
+            addView(ViewUtils.createProposalOpInfoCell(_ctx, uidata, useBuyColorForTitle = true))
+        }
+
+
+        return this
+    }
+}
---
+++ android/app/src/main/java/com/btsplusplus/fowallet/ViewProposalAccountUpdateVoting.kt
@@ -0,0 +1,239 @@
+package com.btsplusplus.fowallet
+
+import android.content.Context
+import android.text.TextUtils
+import android.util.TypedValue
+import android.view.Gravity
+import android.widget.LinearLayout
+import android.widget.TextView
+import bitshares.*
+import com.fowallet.walletcore.bts.ChainObjectManager
+import org.json.JSONArray
+import org.json.JSONObject
+import kotlin.math.max
+import kotlin.math.min
+
+class ViewProposalAccountUpdateVoting : LinearLayout {
+
+    companion object {
+        /**
+        *  计算行显示信息
+        */
+        fun calcLineInfos(old_options_json: JSONObject, new_options_json: JSONObject) : JSONObject {
+
+            val total_keys = JSONObject()
+            
+            val old_voting_account = old_options_json.optString("voting_account",null)
+            val new_voting_account = new_options_json.optString("voting_account",null)
+            assert(old_voting_account != null && new_voting_account != null);
+            val old_is_self = old_voting_account.equals(BTS_GRAPHENE_PROXY_TO_SELF)
+            val new_is_self = new_voting_account.equals(BTS_GRAPHENE_PROXY_TO_SELF)
+            
+            if (old_is_self && new_is_self){
+                //  1、更新前后都无代理：更新投票信息对比投票差异
+                val old_vote_ids = JSONObject()
+                val new_vote_ids = JSONObject()
+
+                old_options_json.getJSONArray("votes").forEach<String> { vote_id ->
+                    old_vote_ids.put(vote_id, true)
+                }
+                new_options_json.getJSONArray("votes").forEach<String> { vote_id ->
+                    new_vote_ids.put(vote_id, true)
+                }
+                new_vote_ids.keys().toJSONArray().forEach<String> { vote_id ->
+                    if (!old_vote_ids.getBoolean(vote_id)){
+                        //  新增
+                        total_keys.put(vote_id, JSONObject().apply{
+                            put("isvote", true)
+                            put("isadd", true)
+                        })
+                    }
+                }
+                old_vote_ids.keys().toJSONArray().forEach<String> { vote_id ->
+                    if (!new_vote_ids.getBoolean(vote_id)){
+                        //  删除
+                        total_keys.put(vote_id, JSONObject().apply{
+                            put("isvote", true)
+                            put("isadd", false)
+                        })
+                    }
+                }
+            }else if (!old_is_self && new_is_self){
+                //  2、取消代理：所有的投票都属于新增。
+                total_keys.put("kRemoveProxy",JSONObject().apply {
+                    put("isremoveproxy",true)
+                    put("voting_account",old_voting_account)
+                })
+
+                new_options_json.getJSONArray("votes").forEach<String> { vote_id ->
+                    total_keys.put(vote_id,JSONObject().apply {
+                        put("isvote", true)
+                        put("isadd", true)
+                    })
+                }
+            }else if (old_is_self && !new_is_self){
+                //  3、新增代理：投票信息根据代理人而定（自己的不显示）
+                total_keys.put("kAddProxy", JSONObject().apply{
+                    put("isaddproxy",true)
+                    put("voting_account", new_voting_account)
+                })
+            }else if (!old_is_self && !new_is_self){
+                //  4、更新代理
+                if (!old_voting_account.equals(new_voting_account)){
+                    total_keys.put("kRemoveProxy",JSONObject().apply{
+                        put("isremoveproxy", true)
+                        put("voting_account", old_voting_account)
+                    })
+                    total_keys.put("kAddProxy",JSONObject().apply{
+                        put("isaddproxy", true)
+                        put("voting_account", new_voting_account)
+                    })
+
+                }
+            }
+            return total_keys
+        }
+    }
+
+    var _ctx: Context
+    val content_fontsize = 11.0f
+
+    constructor(ctx: Context) : super(ctx) {
+        _ctx = ctx
+        val layout_params = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT)
+        layout_params.topMargin = 10
+        this.layoutParams = layout_params
+        this.orientation = LinearLayout.VERTICAL
+    }
+
+    private fun genLineLables(name: String, result: String, color_name: Int, color_result: Int) : LinearLayout {
+        val layout = LinearLayout(_ctx)
+        layout.layoutParams = LinearLayout.LayoutParams(LLAYOUT_MATCH, LLAYOUT_WARP).apply {
+            setMargins(0,2.dp,0, 2.dp)
+        }
+        layout.orientation = LinearLayout.HORIZONTAL
+        layout.gravity = Gravity.CENTER_VERTICAL
+        layout.apply {
+            val tv_name = TextView(_ctx).apply {
+                layoutParams = LinearLayout.LayoutParams(0, LLAYOUT_WARP,6f)
+                gravity = Gravity.LEFT or Gravity.CENTER_VERTICAL
+                setTextColor(resources.getColor(color_name))
+                setTextSize(TypedValue.COMPLEX_UNIT_DIP, content_fontsize)
+                setSingleLine(true)
+                ellipsize = TextUtils.TruncateAt.valueOf("END")
+                text = name
+            }
+            val tv_result = TextView(_ctx).apply {
+                layoutParams = LinearLayout.LayoutParams(0, LLAYOUT_WARP,1f).apply {
+                    gravity = Gravity.RIGHT or Gravity.CENTER_VERTICAL
+                }
+                gravity = Gravity.RIGHT or Gravity.CENTER_VERTICAL
+                setTextColor(resources.getColor(color_result))
+                setTextSize(TypedValue.COMPLEX_UNIT_DIP, content_fontsize)
+                text = result
+            }
+            addView(tv_name)
+            addView(tv_result)
+        }
+        return layout
+    }
+
+    private fun genLablesFromLineInfo(line: JSONObject, vote_id: String?) : LinearLayout {
+        val chainMgr = ChainObjectManager.sharedChainObjectManager()
+        var name: String
+        var status: String
+        val name_color = R.color.theme01_textColorNormal
+        val status_color: Int
+        if (line.optBoolean("isvote") == true){
+            val vote_info = chainMgr.getVoteInfoByVoteID(vote_id!!)
+            val committee_member_account = vote_info!!.optString("committee_member_account", null)
+            if (committee_member_account != null){
+                name = String.format("%s %s", R.string.kOpDetailSubPrefixCommittee.xmlstring(_ctx), chainMgr.getChainObjectByID(committee_member_account).getString("name"))
+            }else{
+                val witness_account = vote_info.optString("witness_account",null)
+                if (witness_account != null){
+                    name = String.format("%s %s", R.string.kOpDetailSubPrefixWitness.xmlstring(_ctx), chainMgr.getChainObjectByID(witness_account).getString("name"))
+                }else{
+                    name = String.format("%s %s", vote_info.getString("id"), vote_info.getString("name"))
+                }
+            }
+            name = String.format("* %s",name)
+            if (line.getBoolean("isadd")){
+                status = R.string.kOpDetailSubOpAdd.xmlstring(_ctx)
+                status_color = R.color.theme01_buyColor
+            }else{
+                status = R.string.kOpDetailSubOpDelete.xmlstring(_ctx)
+                status_color = R.color.theme01_sellColor
+            }
+        }else{
+            if (line.optBoolean("isremoveproxy") == true){
+                name = String.format("* %s %s", R.string.kOpDetailSubPrefixProxy.xmlstring(_ctx), chainMgr.getChainObjectByID(line.getString("voting_account")).getString("name"))
+                status = R.string.kOpDetailSubOpDelete.xmlstring(_ctx)
+                status_color = R.color.theme01_sellColor
+            } else {
+                name = String.format("* %s %s", R.string.kOpDetailSubPrefixProxy.xmlstring(_ctx), chainMgr.getChainObjectByID(line.getString("voting_account")).getString("name"))
+                status = R.string.kOpDetailSubOpAdd.xmlstring(_ctx)
+                status_color = R.color.theme01_buyColor
+            }
+        }
+
+        return genLineLables(name, status, name_color, status_color)
+
+    }
+
+    /**
+     *  (private) 优先按照vote_type升序排列，vote_type相同则按照vote_id升序排列。
+     */
+    private fun _sort_votes(votes: JSONArray) : JSONArray {
+        return votes.toList<String>().sortedWith(Comparator<String>{ obj1, obj2 ->
+            val ary1 = obj1.split(":")
+            val ary2 = obj1.split(":")
+            val vote_type_1 = ary1.first().toInt()
+            val vote_type_2 = ary2.first().toInt()
+            if (vote_type_1 == vote_type_2) {
+                val vote_id_1 = ary1.last().toInt()
+                val vote_id_2 = ary2.last().toInt()
+                if (vote_id_1 < vote_id_2){
+                    return@Comparator 1
+                } else if (vote_id_1 > vote_id_2){
+                    return@Comparator -1
+                } else {
+                    return@Comparator 0
+                }
+            } else {
+                 if (vote_type_1 < vote_type_2){
+                     return@Comparator 1
+                 }else{
+                     return@Comparator -1
+                 }
+            }
+        }).toJsonArray()
+    }
+
+    fun initWithOptions(old_options_json: JSONObject?, new_options_json: JSONObject?): LinearLayout {
+
+        val _old_options_json = old_options_json
+        val _new_options_json = new_options_json
+
+        assert(_old_options_json != null && new_options_json != null)
+
+        val lines = calcLineInfos(old_options_json!!, new_options_json!!)
+
+        addView(genLineLables(R.string.kOpDetailSubTitleVoteTargeter.xmlstring(_ctx), R.string.kOpDetailSubTitleOperate.xmlstring(_ctx), R.color.theme01_textColorGray, R.color.theme01_textColorGray))
+
+        val removeProxy = lines.optJSONObject("kRemoveProxy")
+        if (removeProxy != null){
+            lines.remove("kRemoveProxy")
+            addView(genLablesFromLineInfo(removeProxy,null))
+        }
+        val addProxy = lines.optJSONObject("kAddProxy")
+        if (addProxy != null){
+            lines.remove("kAddProxy")
+            addView(genLablesFromLineInfo(addProxy,null))
+        }
+        _sort_votes(lines.keys().toJSONArray()).forEach<String>{ vote_id ->
+            addView(genLablesFromLineInfo(lines.getJSONObject(vote_id),vote_id))
+        }
+        return this
+    }
+}
