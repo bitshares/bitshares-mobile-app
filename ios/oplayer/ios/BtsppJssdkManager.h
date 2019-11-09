e7de51418df72ce5cbd614aca9a441f242a1dd8a
syalon <hanomirin@foxmail.com>
2019-11-10 01:40:49 +0800

    ios: btsppsdk jsb add test api.

--- ios/oplayer/ios/BtsppJssdkManager.h
+++ ios/oplayer/ios/BtsppJssdkManager.h
@@ -9,10 +9,13 @@
 #import <Foundation/Foundation.h>
 #import "WsPromise.h"
 
+@class VCBase;
+
 @interface BtsppJssdkManager : NSObject
 
 + (BtsppJssdkManager*)sharedBtsppJssdkManager;
 
+- (BtsppJssdkManager*)binding_vc:(VCBase*)vc;
 - (id)js_call:(NSString*)method args:(id)args;
 
 @end
