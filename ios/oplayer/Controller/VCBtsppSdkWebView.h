e9044138506ac632cc022b021bfc0fd46c7ce6f2
syalon <hanomirin@foxmail.com>
2019-11-09 10:06:09 +0800

    ios: init btsppsdk webview base.

---
+++ ios/oplayer/Controller/VCBtsppSdkWebView.h
@@ -0,0 +1,18 @@
+//
+//  VCBtsppSdkWebView.h
+//  oplayer
+//
+//  Created by 夏 胧 on 14-3-25.
+//
+//
+
+#import "VCBase.h"
+#import <WebKit/WebKit.h>
+
+@interface VCBtsppSdkWebView : VCBase<WKNavigationDelegate>
+
+- (id)initWithUrl:(NSString*)url;
+- (void)reload;
+- (BOOL)goBack;
+
+@end
