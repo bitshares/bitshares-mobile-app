21e0cd38e46ac3bd3b2b69cc317c07e5b1e945ff
qingfengzh <yoshikizh@126.com>
2019-11-26 10:23:26 +0800

    android: added otc trade modal view

--- android/app/src/main/java/com/btsplusplus/fowallet/ViewLine.kt
+++ android/app/src/main/java/com/btsplusplus/fowallet/ViewLine.kt
@@ -7,9 +7,9 @@ import bitshares.dp
 
 class ViewLine : View {
 
-    constructor(context: Context, margin_top: Int = 0, margin_bottom: Int = 0) : super(context) {
+    constructor(context: Context, margin_top: Int = 0, margin_bottom: Int = 0, margin_left: Int = 0, margin_right: Int = 0) : super(context) {
         var layout_line_params = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, 1.dp)
-        layout_line_params.setMargins(0, margin_top, 0, margin_bottom)
+        layout_line_params.setMargins(margin_left, margin_top, margin_right, margin_bottom)
         this.setBackgroundColor(resources.getColor(R.color.theme01_bottomLineColor))
         this.layoutParams = layout_line_params
     }
