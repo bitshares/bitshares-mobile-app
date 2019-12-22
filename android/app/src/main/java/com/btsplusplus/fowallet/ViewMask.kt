774f05324feb97ecb9fa3c746b75112589f6406e
qingfengzh <yoshikizh@126.com>
2019-12-22 14:59:11 +0800

    android: replace loading mask effect

--- android/app/src/main/java/com/btsplusplus/fowallet/ViewMask.kt
+++ android/app/src/main/java/com/btsplusplus/fowallet/ViewMask.kt
@@ -1,21 +1,30 @@
 package com.btsplusplus.fowallet
 
+import android.annotation.SuppressLint
+import android.app.ActionBar
 import android.app.Activity
 import android.app.Dialog
 import android.content.Context
 import android.graphics.drawable.ClipDrawable
 import android.graphics.drawable.ColorDrawable
-import android.view.Gravity
-import android.view.KeyEvent
-import android.view.View
+import android.view.*
 import android.widget.LinearLayout
 import android.widget.ProgressBar
 import android.widget.TextView
+import bitshares.LLAYOUT_MATCH
+import bitshares.LLAYOUT_WARP
+import bitshares.dp
+import bitshares.px2dp
+import com.btsplusplus.fowallet.indictorView.AVLoadingIndicatorView
+import org.xmlpull.v1.XmlPullParser
+
+
 
 class ViewMask : Dialog {
 
     private var _owner: Context? = null
 
+    @SuppressLint("ResourceType")
     constructor(loading_text: String, context: Context?) : super(context) {
         _owner = context
 
@@ -24,21 +33,26 @@ class ViewMask : Dialog {
         textView.setTextColor(context.resources.getColor(R.color.theme01_textColorMain))
         textView.gravity = Gravity.CENTER
 
-        val progress = ProgressBar(context)
-
-        // Todo progress 变色不起作用
-        val drawable = ClipDrawable(ColorDrawable(context.resources.getColor(R.color.theme01_textColorMain)), Gravity.LEFT, ClipDrawable.HORIZONTAL)
-        progress.progressDrawable = drawable
+        val layout_loading = LinearLayout(context).apply {
+            layoutParams = LinearLayout.LayoutParams(LLAYOUT_MATCH, LLAYOUT_WARP).apply {
+                gravity = Gravity.CENTER
+            }
+            gravity = Gravity.CENTER
+        }
+        LayoutInflater.from(context).inflate(R.layout.activity_loading_view, layout_loading)
 
         val layout = LinearLayout(context)
-        val layout_params = LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT)
+        val layout_params = LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT).apply {
+            gravity = Gravity.CENTER
+        }
         layout.orientation = LinearLayout.VERTICAL
         layout.layoutParams = layout_params
+        layout.gravity = Gravity.CENTER
 
-        layout.setBackgroundColor(context.resources.getColor(R.color.theme01_textColorGray))
-        layout.setPadding(20, 20, 20, 20)
+        layout.background = context.resources.getDrawable(R.drawable.loading)
+        layout.setPadding(5.dp, 14.dp, 5.dp, 20.dp)
 
-        layout.addView(progress)
+        layout.addView(layout_loading)
         layout.addView(textView)
 
         setContentView(layout)
@@ -52,6 +66,7 @@ class ViewMask : Dialog {
         attr.width = LinearLayout.LayoutParams.WRAP_CONTENT
         attr.title = null
         attr.height = LinearLayout.LayoutParams.WRAP_CONTENT
+        this.window.setBackgroundDrawableResource(R.color.transparent)
         this.window.attributes = attr
 
         // REMARK 部分机型去除标题栏
