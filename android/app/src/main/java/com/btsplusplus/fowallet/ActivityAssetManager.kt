6029aa4fd0710745244828c21fe5fccab24a6e7c
syalon <hanomirin@foxmail.com>
2020-02-11 18:45:32 +0800

    android: update asset manager interface.

--- android/app/src/main/java/com/btsplusplus/fowallet/ActivityAssetManager.kt
+++ android/app/src/main/java/com/btsplusplus/fowallet/ActivityAssetManager.kt
@@ -1,17 +1,15 @@
 package com.btsplusplus.fowallet
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.LinearLayout
-import android.widget.TextView
-import bitshares.forEach
+import bitshares.*
+import com.btsplusplus.fowallet.utils.VcUtils
+import com.fowallet.walletcore.bts.ChainObjectManager
+import com.fowallet.walletcore.bts.WalletManager
 import kotlinx.android.synthetic.main.activity_asset_manager.*
 import org.json.JSONArray
 import org.json.JSONObject
 
 class ActivityAssetManager : BtsppActivity() {
-
-    lateinit var _layout_wrap_from_assets_manager: LinearLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,38 +19,188 @@ class ActivityAssetManager : BtsppActivity() {
         //  设置全屏(隐藏状态栏和虚拟导航栏)
         setFullScreen()
 
-        _layout_wrap_from_assets_manager = layout_wrap_from_assets_manager
-
+        //  事件 - 创建资产
         button_add_from_assets_manager.setOnClickListener {
-            goTo(ActivityAssetCreateOrEdit::class.java, true)
+            val result_promise = Promise()
+            goTo(ActivityAssetCreateOrEdit::class.java, true, args = JSONObject().apply {
+                put("kTitle", resources.getString(R.string.kVcTitleAssetOpCreate))
+                put("result_promise", result_promise)
+            })
+            result_promise.then { dirty ->
+                //  刷新UI
+                if (dirty != null && dirty as Boolean) {
+                    queryMyIssuedAssets()
+                }
+            }
         }
 
-        refreshUI()
+        //  事件 - 返回
+        layout_back_from_assets_manager.setOnClickListener { finish() }
 
-        layout_back_from_assets_manager.setOnClickListener {
-            finish()
+        //  查询
+        queryMyIssuedAssets()
+    }
+
+    private fun queryMyIssuedAssets() {
+        val account_name = WalletManager.sharedWalletManager().getWalletAccountName()!!
+
+        //  TODO:4.0 limit config
+        val chainMgr = ChainObjectManager.sharedChainObjectManager()
+        val mask = ViewMask(resources.getString(R.string.kTipsBeRequesting), this).apply { show() }
+        chainMgr.queryAssetsByIssuer(account_name, "1.${EBitsharesObjectType.ebot_asset.value}.0", 100).then {
+            val issuerHash = JSONObject()
+            val bitasset_data_id_list = JSONArray()
+            val dynamic_asset_data_id_list = JSONArray()
+
+            val data_array = (it as? JSONArray) ?: JSONArray()
+            for (asset in data_array.forin<JSONObject>()) {
+                issuerHash.put(asset!!.getString("issuer"), true)
+                val bitasset_data_id = asset.optString("bitasset_data_id")
+                if (bitasset_data_id.isNotEmpty()) {
+                    bitasset_data_id_list.put(bitasset_data_id)
+                }
+                val dynamic_asset_data_id = asset.getString("dynamic_asset_data_id")
+                dynamic_asset_data_id_list.put(dynamic_asset_data_id)
+            }
+
+            //  全部都查询都忽略缓存
+            bitasset_data_id_list.putAll(issuerHash.keys().toJSONArray())
+            val p1 = chainMgr.queryAllGrapheneObjectsSkipCache(bitasset_data_id_list)
+            val p2 = chainMgr.queryAllGrapheneObjectsSkipCache(dynamic_asset_data_id_list)
+            return@then Promise.all(p1, p2).then {
+                mask.dismiss()
+                onQueryMyIssuedAssetsResponsed(data_array)
+                return@then null
+            }
+        }.catch {
+            mask.dismiss()
+            showToast(resources.getString(R.string.tip_network_error))
         }
     }
 
-    private fun refreshUI(){
+    private fun onQueryMyIssuedAssetsResponsed(data_array: JSONArray) {
+        drawUI(data_array)
+    }
 
-        val data = JSONArray().apply {
-            for (i in 0 until 10){
+    private fun drawUI(data_array: JSONArray) {
+        lay_cell_container.let { layout ->
+            layout.removeAllViews()
+            if (data_array.length() > 0) {
+                data_array.forEach<JSONObject> {
+                    val asset = it!!
+                    val v = ViewAssetCell(this, asset)
+                    v.setOnClickListener { onCellClicked(asset) }
+                    layout.addView(v)
+                }
+            } else {
+                layout.addView(ViewUtils.createEmptyCenterLabel(this, resources.getString(R.string.kVcAssetMgrEmptyList)))
+            }
+        }
+    }
+
+    private fun onCellClicked(asset: JSONObject) {
+        val chainMgr = ChainObjectManager.sharedChainObjectManager()
+        val bitasset_data_id = asset.optString("bitasset_data_id")
+        var bitasset_data: JSONObject? = null
+        if (bitasset_data_id.isNotEmpty()) {
+            bitasset_data = chainMgr.getChainObjectByID(bitasset_data_id)
+        }
+
+        val self = this
+        val list = JSONArray().apply {
+            //  TODO:5.0 暂时去掉
+//            put(JSONObject().apply {
+//                put("type", EBitsharesAssetOpKind.ebaok_view)
+//                put("title", self.resources.getString(R.string.kVcAssetMgrCellActionViewDetail))
+//            })
+            put(JSONObject().apply {
+                put("type", EBitsharesAssetOpKind.ebaok_edit)
+                put("title", self.resources.getString(R.string.kVcAssetMgrCellActionUpdateAsset))
+            })
+            if (bitasset_data != null) {
                 put(JSONObject().apply {
-
-                    put("asset_symbol", String.format("CNY_%s",i.toString()))
-                    put("asset_name", String.format("China_yuan_%s",i.toString()))
-                    put("asset_quantity", i.toString())
-                    put("supply","41,731,117.9935")
-                    put("max_supply","100,000,000,000")
-                    put("privacy_supply","363.3342")
-
+                    put("type", EBitsharesAssetOpKind.ebaok_update_bitasset)
+                    put("title", self.resources.getString(R.string.kVcAssetMgrCellActionUpdateBitasset))
+                })
+            } else {
+                put(JSONObject().apply {
+                    put("type", EBitsharesAssetOpKind.ebaok_issue)
+                    put("title", self.resources.getString(R.string.kVcAssetMgrCellActionIssueAsset))
                 })
             }
         }
 
-        data.forEach<JSONObject> {
-            _layout_wrap_from_assets_manager.addView(ViewAssetCell(this, it!!))
+        ViewSelector.show(this, "", list, key = "title") { index: Int, _: String ->
+            val item = list.getJSONObject(index)
+            when (item.get("type") as EBitsharesAssetOpKind) {
+                EBitsharesAssetOpKind.ebaok_view -> {
+                    //  TODO:5.0暂不支持
+                }
+                EBitsharesAssetOpKind.ebaok_edit -> {
+                    //  查询黑白名单中各种ID依赖。编辑黑白名单列表需要显示名称。
+                    val options = asset.getJSONObject("options")
+                    val ids_hash = JSONObject()
+                    for (oid in options.getJSONArray("whitelist_authorities").forin<String>()) {
+                        ids_hash.put(oid, true)
+                    }
+                    for (oid in options.getJSONArray("blacklist_authorities").forin<String>()) {
+                        ids_hash.put(oid, true)
+                    }
+                    for (oid in options.getJSONArray("whitelist_markets").forin<String>()) {
+                        ids_hash.put(oid, true)
+                    }
+                    for (oid in options.getJSONArray("blacklist_markets").forin<String>()) {
+                        ids_hash.put(oid, true)
+                    }
+                    VcUtils.simpleRequest(this, chainMgr.queryAllGrapheneObjects(ids_hash.keys().toJSONArray())) {
+                        val result_promise = Promise()
+                        goTo(ActivityAssetCreateOrEdit::class.java, true, args = JSONObject().apply {
+                            put("kEditAsset", asset)
+                            put("kTitle", self.resources.getString(R.string.kVcTitleAssetOpUpdateBasic))
+                            put("result_promise", result_promise)
+                        })
+                        result_promise.then { dirty ->
+                            //  刷新UI
+                            if (dirty != null && dirty as Boolean) {
+                                queryMyIssuedAssets()
+                            }
+                        }
+                    }
+                }
+                EBitsharesAssetOpKind.ebaok_update_bitasset -> {
+                    //  查询背书资产名称依赖
+                    VcUtils.guardGrapheneObjectDependence(this, bitasset_data!!.getJSONObject("options").getString("short_backing_asset")) {
+                        val result_promise = Promise()
+                        goTo(ActivityAssetCreateOrEdit::class.java, true, args = JSONObject().apply {
+                            put("kEditAsset", asset)
+                            put("kEditBitAsset", bitasset_data)
+                            put("kTitle", self.resources.getString(R.string.kVcTitleAssetOpUpdateBitasset))
+                            put("result_promise", result_promise)
+                        })
+                        result_promise.then { dirty ->
+                            //  刷新UI
+                            if (dirty != null && dirty as Boolean) {
+                                queryMyIssuedAssets()
+                            }
+                        }
+                    }
+                }
+                EBitsharesAssetOpKind.ebaok_issue -> {
+                    val result_promise = Promise()
+                    goTo(ActivityAssetOpissue::class.java, true, args = JSONObject().apply {
+                        put("kAsset", asset)
+                        put("kDynamicAssetData", chainMgr.getChainObjectByID(asset.getString("dynamic_asset_data_id")))
+                        put("kTitle", self.resources.getString(R.string.kVcTitleAssetOpIssue))
+                        put("result_promise", result_promise)
+                    })
+                    result_promise.then { dirty ->
+                        //  刷新UI
+                        if (dirty != null && dirty as Boolean) {
+                            queryMyIssuedAssets()
+                        }
+                    }
+                }
+            }
         }
 
     }
--- android/app/src/main/java/com/btsplusplus/fowallet/ViewAssetCell.kt
+++ android/app/src/main/java/com/btsplusplus/fowallet/ViewAssetCell.kt
@@ -1,12 +1,14 @@
 package com.btsplusplus.fowallet
 
 import android.content.Context
+import android.text.TextUtils
 import android.util.TypedValue
 import android.view.Gravity
 import android.view.View
 import android.widget.LinearLayout
 import android.widget.TextView
-import bitshares.Utils
+import bitshares.*
+import com.fowallet.walletcore.bts.ChainObjectManager
 import org.json.JSONObject
 
 
@@ -26,108 +28,146 @@ class ViewAssetCell : LinearLayout {
         val ctx = _ctx
         val data = _data
 
+        //  初始化数据
+        val chainMgr = ChainObjectManager.sharedChainObjectManager()
+        val asset_id = _data.getString("id")
+        val bitasset_data_id = _data.optString("bitasset_data_id")
+        val bIsCore = chainMgr.grapheneCoreAssetID == asset_id
+        var bIsSmart = false
+        var bIsPrediction = false
+        if (!bIsCore) {
+            if (bitasset_data_id.isNotEmpty()) {
+                if (chainMgr.getChainObjectByID(bitasset_data_id).isTrue("is_prediction_market")) {
+                    bIsPrediction = true
+                } else {
+                    bIsSmart = true
+                }
+            }
+        }
+
+        val asset_options = _data.getJSONObject("options")
+        val precision = _data.getInt("precision")
+        val dynamic_asset_data = chainMgr.getChainObjectByID(_data.getString("dynamic_asset_data_id"))
+
+        var description = asset_options.optString("description")
+        val description_json = description.to_json_object()
+        if (description_json != null) {
+            val main_desc = description_json.optString("main", null)
+            if (main_desc != null) {
+                description = main_desc
+            }
+        }
+
         val layout_params = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, Utils.toDp(24f, _ctx.resources))
         layout_params.gravity = Gravity.CENTER_VERTICAL
 
-        val ly_wrap: LinearLayout = LinearLayout(ctx)
+        val ly_wrap = LinearLayout(ctx)
         ly_wrap.orientation = LinearLayout.VERTICAL
 
         // layout1 左: Buy SEED/CNY 右: 07-11 11:50
-        val ly1: LinearLayout = LinearLayout(ctx)
+        val ly1 = LinearLayout(ctx)
         ly1.orientation = LinearLayout.HORIZONTAL
         ly1.layoutParams = layout_params
         ly1.setPadding(0, Utils.toDp(5.0f, _ctx.resources), 0, 0)
         val tv1 = TextView(ctx)
 
-        tv1.text = _data.getString("asset_symbol")
+        tv1.text = _data.getString("symbol")
         tv1.setTextColor(resources.getColor(R.color.theme01_textColorMain))
         tv1.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 12.0f)
         tv1.gravity = Gravity.CENTER_VERTICAL
         tv1.setPadding(0, 0, Utils.toDp(5.0f, _ctx.resources), 0)
 
-        val tv2 = TextView(ctx)
-        tv2.text = "Smart"
-        tv2.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 11.0f)
-        tv2.setTextColor(resources.getColor(R.color.theme01_textColorMain))
-        tv2.gravity = Gravity.CENTER_VERTICAL
-        tv2.setBackgroundColor(resources.getColor(R.color.theme01_textColorHighlight))
-        tv2.setPadding(Utils.toDp(5.0f, _ctx.resources), 0, Utils.toDp(5.0f, _ctx.resources), 0)
+        //  类型标签
+        var tv2: TextView? = null
+        if (bIsCore || bIsSmart || bIsPrediction) {
+            tv2 = TextView(ctx)
+            tv2.text = if (bIsCore) "Core" else if (bIsSmart) "Smart" else "Prediction"
+            tv2.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 11.0f)
+            tv2.setTextColor(resources.getColor(R.color.theme01_textColorMain))
+            tv2.gravity = Gravity.CENTER_VERTICAL
+            tv2.setBackgroundColor(resources.getColor(R.color.theme01_textColorHighlight))
+            tv2.setPadding(Utils.toDp(5.0f, _ctx.resources), 0, Utils.toDp(5.0f, _ctx.resources), 0)
+        }
 
         // layout2 左: 供应量 中 最大供应量 右 隐私供应量
-        val ly2: LinearLayout = LinearLayout(ctx)
+        val ly2 = LinearLayout(ctx)
         ly2.orientation = LinearLayout.HORIZONTAL
         ly2.layoutParams = layout_params
 
         val tv4 = TextView(ctx)
-        tv4.text = "供应量"
+        tv4.text = R.string.kVcAssetMgrCellInfoCurSupply.xmlstring(ctx)
         tv4.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 12.0f)
         tv4.setTextColor(resources.getColor(R.color.theme01_textColorGray))
         tv4.gravity = Gravity.CENTER_VERTICAL or Gravity.LEFT
         tv4.layoutParams = createLayout(Gravity.CENTER_VERTICAL or Gravity.LEFT)
 
         val tv5 = TextView(ctx)
-        tv5.text = "最大供应量"
+        tv5.text = R.string.kVcAssetMgrCellInfoMaxSupply.xmlstring(ctx)
         tv5.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 12.0f)
         tv5.setTextColor(resources.getColor(R.color.theme01_textColorGray))
         tv5.gravity = Gravity.CENTER_VERTICAL or Gravity.CENTER
         tv5.layoutParams = createLayout(Gravity.CENTER_VERTICAL or Gravity.CENTER)
 
         val tv6 = TextView(ctx)
-        tv6.text = "隐私供应量"
+        tv6.text = R.string.kVcAssetMgrCellInfoConSupply.xmlstring(ctx)
         tv6.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 12.0f)
         tv6.setTextColor(resources.getColor(R.color.theme01_textColorGray))
         tv6.gravity = Gravity.CENTER_VERTICAL or Gravity.RIGHT
         tv6.layoutParams = createLayout(Gravity.CENTER_VERTICAL or Gravity.RIGHT)
 
-
         // layout3
-        val ly3: LinearLayout = LinearLayout(ctx)
+        val ly3 = LinearLayout(ctx)
         ly3.orientation = LinearLayout.HORIZONTAL
         ly3.layoutParams = layout_params
 
         val tv7 = TextView(ctx)
-        tv7.text = data.getString("supply")
+        tv7.text = OrgUtils.formatFloatValue(bigDecimalfromAmount(dynamic_asset_data.getString("current_supply"), precision).toDouble(), precision, has_comma = true)
         tv7.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 12.0f)
         tv7.setTextColor(resources.getColor(R.color.theme01_textColorNormal))
         tv7.gravity = Gravity.CENTER_VERTICAL or Gravity.LEFT
         tv7.layoutParams = createLayout(Gravity.CENTER_VERTICAL or Gravity.LEFT)
 
         val tv8 = TextView(ctx)
-        tv8.text = data.getString("max_supply")
+        tv8.text = OrgUtils.formatFloatValue(bigDecimalfromAmount(asset_options.getString("max_supply"), precision).toDouble(), precision, has_comma = true)
         tv8.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 12.0f)
         tv8.setTextColor(resources.getColor(R.color.theme01_textColorNormal))
         tv8.gravity = Gravity.CENTER_VERTICAL or Gravity.CENTER
         tv8.layoutParams = createLayout(Gravity.CENTER_VERTICAL or Gravity.CENTER)
 
         val tv9 = TextView(ctx)
-        tv9.text = data.getString("privacy_supply")
+        tv9.text = OrgUtils.formatFloatValue(bigDecimalfromAmount(dynamic_asset_data.getString("confidential_supply"), precision).toDouble(), precision, has_comma = true)
         tv9.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 12.0f)
         tv9.setTextColor(resources.getColor(R.color.theme01_textColorNormal))
         tv9.gravity = Gravity.CENTER_VERTICAL or Gravity.RIGHT
         tv9.layoutParams = createLayout(Gravity.CENTER_VERTICAL or Gravity.RIGHT)
 
         // layout4
-        val ly4: LinearLayout = LinearLayout(ctx)
+        val ly4 = LinearLayout(ctx)
         ly4.orientation = LinearLayout.HORIZONTAL
         ly4.layoutParams = layout_params
 
         val tv10 = TextView(ctx)
-        tv10.text = String.format("%s %s", data.getString("asset_quantity"), data.getString("asset_name"))
+        tv10.text = description
         tv10.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 12.0f)
         tv10.setTextColor(resources.getColor(R.color.theme01_textColorMain))
         tv10.gravity = Gravity.CENTER_VERTICAL or Gravity.LEFT
         tv10.layoutParams = createLayout(Gravity.CENTER_VERTICAL or Gravity.LEFT)
-
+        //  单行 尾部截断
+        tv10.setSingleLine(true)
+        tv10.maxLines = 1
+        tv10.ellipsize = TextUtils.TruncateAt.END
 
         // 线
         val lv_line = View(ctx)
-        var layout_tv9 = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, Utils.toDp(1.0f, _ctx.resources))
+        val layout_tv9 = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, Utils.toDp(1.0f, _ctx.resources))
         layout_tv9.setMargins(0, Utils.toDp(10.0f, _ctx.resources), 0, Utils.toDp(10.0f, _ctx.resources))
         lv_line.setBackgroundColor(resources.getColor(R.color.theme01_bottomLineColor))
         lv_line.layoutParams = layout_tv9
 
         ly1.addView(tv1)
-        ly1.addView(tv2)
+        if (tv2 != null) {
+            ly1.addView(tv2)
+        }
 
         ly2.addView(tv4)
         ly2.addView(tv5)
@@ -151,7 +191,7 @@ class ViewAssetCell : LinearLayout {
     }
 
     private fun createLayout(gr: Int): LinearLayout.LayoutParams {
-        var layout = LinearLayout.LayoutParams(Utils.toDp(0f, _ctx.resources), Utils.toDp(24.0f, _ctx.resources))
+        val layout = LinearLayout.LayoutParams(Utils.toDp(0f, _ctx.resources), Utils.toDp(24.0f, _ctx.resources))
         layout.weight = 1.0f
         layout.gravity = gr
         return layout
