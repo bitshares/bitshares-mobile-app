eb876028b1348496cdda5057875b2ce57c7de539
syalon <hanomirin@foxmail.com>
2020-02-26 13:54:41 +0800

    ios: improved margin interface experience.

---
+++ ios/oplayer/View/ViewAutoresizeContailerHor.h
@@ -0,0 +1,28 @@
+//
+//  ViewAutoresizeContailerHor.h
+//  oplayer
+//
+//  Created by SYALON on 13-12-31.
+//
+//  水平方向自动跳转尺寸的容器
+
+#import <UIKit/UIKit.h>
+
+@interface ViewAutoresizeContailerHor : UIView
+
+- (void)addSubview:(UIView*)view tag:(NSInteger)tag;
+- (void)resizeFrame;
+
+@property (nonatomic, assign) CGFloat fViewIntervalSpace;   //  子view间隔
+
+@end
+
+@interface TailerViewAssetAndButtons : ViewAutoresizeContailerHor
+
+- (id)initWithHeight:(CGFloat)fHeight asset_name:(NSString*)asset_name button_names:(NSArray*)button_names target:(id)target action:(SEL)action;
+
+- (void)drawAssetName:(NSString*)asset_name;
+
+@property (nonatomic, strong) UILabel* lbAssetName;
+
+@end
