6e20c3925b9e995ab67afdeabd870553e2d3b1e5
syalon <hanomirin@foxmail.com>
2020-03-10 16:24:47 +0800

    ios: adjust the percent slider of the trading interface to the percent button.

---
+++ ios/oplayer/View/ViewTradePercentButtonCell.h
@@ -0,0 +1,17 @@
+//
+//  ViewTradePercentButtonCell.h
+//  oplayer
+//
+//  Created by SYALON on 13-12-28.
+//
+//
+
+#import <UIKit/UIKit.h>
+#import "UITableViewCellBase.h"
+
+@class VCBase;
+@interface ViewTradePercentButtonCell : UITableViewCellBase
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier vc:(VCBase*)vc;
+
+@end
